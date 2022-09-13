e6f8613ccc566ca1c719b829c2ade030624a4b74
Rancor38 <zak.rancor@hotmail.com>
2022-09-13 13:47:21 -0700

    Working on templates

--- dice_roller_app/urls.py
+++ dice_roller_app/urls.py
@@ -1,9 +1,8 @@
 from django.urls import path
-
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    # path('', views.index, name='index'),
     #Route added for main_app urls
     path('', views.Home.as_view(), name="home"),
 ]
