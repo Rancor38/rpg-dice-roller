f78996c02c649e63531709be208016fad50c3433
Rancor38 <zak.rancor@hotmail.com>
2022-11-21 13:55:04 -0800

    Edit for deploy to railway

--- rpg_dice_roller/settings.py
+++ rpg_dice_roller/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-njv4^dl2thnwyb^9kpuvkf&#&-)lvc57c#ukik*just=!5@=hl'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
